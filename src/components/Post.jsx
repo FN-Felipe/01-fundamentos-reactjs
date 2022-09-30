195423e3ff12aab434467f8917db041183fd4553
Felipe de Assis <felipeassis@hotmail.com>
2022-09-30 19:08:04 -0300

    #

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post(){
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/fn-felipe.png" alt="" />
+                    <Avatar src="https://github.com/maykbrito.png"/>
                     <div className={styles.authorInfo}>
                         <strong>Felipe de Assis</strong>
                         <span>Web Developer</span>
