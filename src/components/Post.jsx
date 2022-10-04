55db5cae2357d38c9a917c858e994c2a8b3e887e
Felipe de Assis <felipeassis@hotmail.com>
2022-10-04 07:43:59 -0300

    #

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,8 +29,22 @@ export function Post({author, publishedAt, content}){
     }
 
     function handleNewCommentChange(){
+        event.target.setCustomValidity('')
         setNewCommentText(event.target.value);
-    }  
+    }
+
+    function deleteComment(commentToDelete){
+        const commentsWithoutDeleteOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+        setComments(commentsWithoutDeleteOne);
+    }
+
+    function handleNewCommentInvaled(){
+        event.target.setCustomValidity('Esse campo é obrigatório!')
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0
 
     return (
         <article className={styles.post}>
@@ -51,9 +65,9 @@ export function Post({author, publishedAt, content}){
             <div className={styles.content}>
                 {content.map(line => {
                     if(line.type === 'paragreph'){
-                        return <p>{line.content}</p>
+                        return <p key={line.content}>{line.content}</p>
                     } else if (line.type === 'link'){
-                        return <p><a href='#'>{line.content}</a></p>
+                        return <p key={line.content}><a href='#'>{line.content}</a></p>
                     }
                 })}
             </div>
@@ -66,16 +80,22 @@ export function Post({author, publishedAt, content}){
                     value={newCommentText}
                     placeholder='Deixe um comentário'
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvaled}
+                    required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button 
+                    type='submit'
+                    disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment}/>
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
                 })}
             </div>
 
